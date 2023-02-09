19605eeba0e4cc5d392f523e598d0f1cffdce20b
ChandiniShivanna <chandinigj@gmail.com>
2023-02-09 19:12:42 +0530

    backend Interactions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignIn from './Component/SignIn/SignIn';
 import SignUp from './Component/SignUp/SignUp';
-
+import ProRoutes from './ProtectedRoutes/routes';
 
 
 
@@ -16,7 +16,7 @@ function App() {
               <Route path='/' element={<SignIn/>} />
               <Route path='/login' element={<SignIn />} />
               <Route path='/register' element={<SignUp />} />
-              
+              <Route element={<ProRoutes />}></Route>
              
             </Routes>
           </BrowserRouter>
---
+++ src/ProtectedRoutes/routes.js
@@ -0,0 +1,7 @@
+import { Outlet, Navigate } from "react-router-dom";
+const ProRoutes=()=>{
+    return(
+        localStorage.getItem('token') ? <Outlet/> : <Navigate to = "/login"/>
+    )
+}
+export default ProRoutes;
